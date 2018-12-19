32592eaf4280dd0146078441fd7157607adacca3
kjd <ccc7497@gmail.com>
2018-12-19 10:48:17 +0900

    Exit_button_alarm program

--- 1_startcamp/day2/morning_quiz.py
+++ 1_startcamp/day2/morning_quiz.py
@@ -1,1 +1,20 @@
 print('good morning')
+
+# 1. 평균
+my_score = [79, 84, 66, 93]
+my_average = sum(my_score)/len(my_score)
+print(my_average)
+print(type(my_average))
+
+# 2. 
+your_score = {
+    '수학':87,
+    '국어':83,
+    '영어':76,
+    '도덕':100
+}
+your_average = sum(your_score.values())/len(your_score)
+print(your_average)
+print(type(your_average))
+print(type(your_score.values()))
+
