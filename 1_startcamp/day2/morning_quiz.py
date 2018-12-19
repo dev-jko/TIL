8a2bc44a6b591f8622450e62cc1e9627ad35e3db
kjd <ccc7497@gmail.com>
2018-12-19 12:49:25 +0900

    fix exit_button_alarm

--- 1_startcamp/day2/morning_quiz.py
+++ 1_startcamp/day2/morning_quiz.py
@@ -6,15 +6,45 @@ my_average = sum(my_score)/len(my_score)
 print(my_average)
 print(type(my_average))
 
-# 2. 
+# 2.
 your_score = {
-    '수학':87,
-    '국어':83,
-    '영어':76,
-    '도덕':100
+    '수학': 87,
+    '국어': 83,
+    '영어': 76,
+    '도덕': 100
 }
 your_average = sum(your_score.values())/len(your_score)
 print(your_average)
 print(type(your_average))
 print(type(your_score.values()))
+
+# 3.
+cities_temp = {
+    '서울': [-6, -10, 5],
+    '대전': [-3, -5, 2],
+    '광주': [0, -5, 10],
+    '구미': [2, -2, 9]
+}
+
+# 도시별 온도 평균
+# 서울: ?
+# 대전: ?
+for key, value in cities_temp.items():
+    print(key+':', round(sum(value)/len(value), 1))
+
+
+# 4. 도시중 최근 3일간 가장 추웠던 곳, 가장 더웠던 곳
+# Hottest: ?, Coldest: ?
+hottest = {'name': '', 'temp': -999}
+coldest = {'name': '', 'temp': 999}
+for key, value in cities_temp.items():
+    for temp in value:
+        if hottest['temp'] < temp:
+            hottest['name'] = key
+            hottest['temp'] = temp
+        if coldest['temp'] > temp:
+            coldest['name'] = key
+            coldest['temp'] = temp
+print('Hottest:', hottest['name'])
+print('Coldest:', coldest['name'])
 
--- alarm_Exit/alarm.pyw
+++ alarm_Exit/alarm.pyw
@@ -1,24 +1,30 @@
 import time
 import os
 
-alarm_time = {
-    'hour':18,
-    'minute':2
-}
-current_time = {
-    'hour':time.localtime().tm_hour,
-    'minute':time.localtime().tm_minute
-}
-file_path = 'C:\\Users\\student\\TIL\\alarm_Exit\\alarm_img.png'
+def getCurrentTIme():
+    current_time = {
+        'hour':time.localtime().tm_hour,
+        'minute':time.localtime().tm_min
+    }
+    return current_time
 
-def alarm():
+def alarm(alarm_time):
     while True:
+        current_time = getCurrentTIme()
         if alarm_time['hour'] == current_time['hour']:
             os.startfile(file_path)
             while True:
+                current_time = getCurrentTIme()
                 if alarm_time['minute'] == current_time['minute']:
                     os.startfile(file_path)
                     return
+                time.sleep(10)
+        time.sleep(10)
 
 
-alarm()
+alarm_time = {
+    'hour':18,
+    'minute':3
+}
+file_path = 'C:\\Users\\student\\TIL\\alarm_Exit\\alarm_img.png'
+alarm(alarm_time)
