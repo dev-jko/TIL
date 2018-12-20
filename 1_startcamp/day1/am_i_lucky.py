3aa79d062a83b389e1e860ae07a7bb9167ac334f
kjd <ccc7497@gmail.com>
2018-12-20 14:13:01 +0900

    alarm

--- 1_startcamp/day1/am_i_lucky.py
+++ 1_startcamp/day1/am_i_lucky.py
@@ -1,32 +1,31 @@
 import requests
 import random
 
-bnusNo = 0
-real_numbers = []
-
-def getReal():
-    game = 837
+def get_real_lotto(game_number):
+    real_numbers = {'numbers':[], 'bonus':0}
+    game_number = 837
     url = 'https://www.nlotto.co.kr/common.do?method=getLottoNumber&drwNo='
-    response = requests.get(url+str(game), verify=False).json()
-    bnusNo = response['bnusNo']
+    response = requests.get(url+str(game_number), verify=False).json()
+    real_numbers['bonus'] = response['bnusNo']
     for i in range(1,7):
-        real_numbers.append(response['drwtNo'+str(i)])
-    print('당첨번호 : ', real_numbers, '+ ' + str(bnusNo))
+        real_numbers['numbers'].append(response['drwtNo'+str(i)])
+    print('당첨번호 : ', real_numbers['numbers'], '+ ' + str(real_numbers['bonus']))
+    return real_numbers
 
-def func():
-    my_numbers = None
-    my_numbers = sorted(random.sample(list(range(1,46)), 7))
+def pick_lotto():
+    return sorted(random.sample(list(range(1,46)), 7))
+
+def am_i_lucky(my_numbers, real_numbers):
     print('내 번호 : ', my_numbers)
-
     count = 0
     check = False
     for i in my_numbers:
-        if i in real_numbers:
+        if i in real_numbers['numbers']:
             count += 1
     if count == 6:
         print('1등')
         check = True
-    elif (count == 5 & bnusNo in my_numbers):
+    elif (count == 5 & real_numbers['bonus'] in my_numbers):
         print('2등')
         check = True
     elif count == 5:
@@ -42,7 +41,12 @@ def func():
     return check
 
 
-getReal()
+game_number = int(input('회차 입력 : '))
+real_numbers = get_real_lotto(game_number)
+count = 0
 while(True):
-    if func():
+    my_numbers = pick_lotto()
+    count += 1
+    if am_i_lucky(my_numbers, real_numbers):
         break
+print(count)
--- alarm_Exit/alarm.pyw
+++ alarm_Exit/alarm.pyw
@@ -1,5 +1,8 @@
 import time
 import os
+from pywinauto.application import Application
+from pywinauto.win32functions import SetForegroundWindow
+from pywinauto.win32functions import SetFocus
 
 def getCurrentTIme():
     current_time = {
@@ -8,15 +11,27 @@ def getCurrentTIme():
     }
     return current_time
 
-def alarm(alarm_time):
+def openfile(file_path):
+    # os.startfile(path)
+    # app = Application().connect(path=file_path)
+    app = Application().start('mspaint.exe' + ' ' + file_path)
+    w = app.top_window()
+    # Application().win
+    # SetFocus(w.wrapper_object())
+    # SetForegroundWindow(w.wrapper_object())
+    
+
+def alarm(alarm_time, start_file_path, end_file_path):
+    time.sleep(2)
+    openfile(start_file_path)
     while True:
         current_time = getCurrentTIme()
         if alarm_time['hour'] == current_time['hour']:
-            os.startfile(file_path)
+            openfile(end_file_path)
             while True:
                 current_time = getCurrentTIme()
                 if alarm_time['minute'] == current_time['minute']:
-                    os.startfile(file_path)
+                    openfile(end_file_path)
                     return
                 time.sleep(10)
         time.sleep(10)
@@ -26,5 +41,6 @@ alarm_time = {
     'hour':18,
     'minute':3
 }
-file_path = 'C:\\Users\\student\\TIL\\alarm_Exit\\alarm_img.png'
-alarm(alarm_time)
+start_file_path = r'C:\\Users\\student\\TIL\\alarm_Exit\\start_alarm_img.png'
+end_file_path = r'C:\\Users\\student\\TIL\\alarm_Exit\\end_alarm_img.png'
+alarm(alarm_time, start_file_path, end_file_path)
