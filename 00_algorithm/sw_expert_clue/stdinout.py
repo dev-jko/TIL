1fa8bc6d9553d444f6dce268e53da66ef054322f
kjd <ccc7497@gmail.com>
2019-02-15 18:08:20 +0900

    ss3 | algorithm stdinout | ~190215

--- 00_algorithm/sw_expert_clue/stdinout.py
+++ 00_algorithm/sw_expert_clue/stdinout.py
@@ -1,4 +1,11 @@
-import sys
-sys.stdin = open('input1.txt', 'r')
-sys.stdout = open('output.txt', 'w')
-
+def change(file_name):
+    import sys
+    file_name = file_name[:-3]
+    try:
+        with open(f'{file_name}.txt', 'r') as f:
+            sys.stdin = f
+    except FileNotFoundError:
+        print('input file이 없습니다.')
+        sys.exit()
+    finally:
+        sys.stdout = open(f'{file_name}output.txt', 'w')
