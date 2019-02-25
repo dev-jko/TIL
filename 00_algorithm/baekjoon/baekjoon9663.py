de9d84580c7a1de9b53fc099460decb2b04aad9a
nadarm <ccc7497@gmail.com>
2019-02-25 21:10:50 +0900

    study | algorithm n queen | ~190225

--- 00_algorithm/baekjoon/baekjoon9663.py
+++ 00_algorithm/baekjoon/baekjoon9663.py
@@ -1,11 +1,39 @@
-def bt(n, grid, k, cnt):
+d = [[-1, -1], [-1, 1]]
+
+
+def bt(n, grid, k, result):
     if n == k:
-        return cnt + 1
-    for i in range(n):
-        bt(n, grid, )
+        result[0] += 1
+    else:
+        for i in range(n):
+            check = False
+            for j in range(N):
+                if grid[j][i] == 1 and j != k:
+                    check = True
+                    break
+            if check:
+                continue
+            for value in d:
+                x, y = k, i
+                while True:
+                    x += value[0]
+                    y += value[1]
+                    if not 0 <= x < n or not 0 <= y < n:
+                        break
+                    if grid[x][y] == 1:
+                        check = True
+                        break
+                if check:
+                    break
+            if check:
+                continue
+            grid[k][i] = 1
+            bt(n, grid, k + 1, result)
+            grid[k][i] = 0
 
 
 N = int(input())
 grid = [[0 for _ in range(N)] for _ in range(N)]
-result = bt(N, grid, 0, 0)
-print(result)
+result = [0]
+bt(N, grid, 0, result)
+print(result[0])
