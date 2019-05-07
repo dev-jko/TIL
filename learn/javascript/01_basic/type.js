15205eb8ae1438121a990404a5e942981c322e81
kjd <ccc7497@gmail.com>
2019-05-07 17:57:12 +0900

    ss3 | Vue

---
+++ learn/javascript/01_basic/type.js
@@ -0,0 +1,12 @@
+console.log(typeof 1); // number
+
+typeof (typeof 1); // string
+
+typeof (()=>{}); // function
+
+typeof NaN; // number
+
+typeof undefined; // undefined
+
+typeof null; // object
+
