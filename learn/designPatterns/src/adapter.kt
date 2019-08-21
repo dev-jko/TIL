e83f10bc9b6968cc84357a92e59fe1bf8a801238
nadarm <ccc7497@gmail.com>
2019-08-21 20:00:24 +0900

    adatper pattern

---
+++ learn/designPatterns/src/adapter.kt
@@ -0,0 +1,40 @@
+fun main() {
+    val oldPhone = OldPhone()
+    chargePhone(oldPhone)
+
+    val adapter = Adapter()
+    val newPhone = NewPhone()
+    adapter.phone = newPhone
+    chargePhone(adapter)
+}
+
+fun chargePhone(connectable: Connectable) {
+    connectable.plug220v()
+}
+
+interface Connectable {
+    fun plug220v()
+}
+
+class OldPhone : Connectable {
+    override fun plug220v() {
+        println("old phone charge 220v")
+    }
+}
+
+class NewPhone {
+    fun charge() {
+        println("new phone charge")
+    }
+}
+
+class Adapter(
+    var phone: NewPhone?
+) : Connectable {
+
+    constructor() : this(null)
+
+    override fun plug220v() {
+        phone?.charge()
+    }
+}
