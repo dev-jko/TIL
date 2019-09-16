646f9ee7c5a60128cea26b73328e125afe9fbba4
Jaedoo Ko <ccc7497@gmail.com>
2019-09-16 11:15:00 +0900

    swift

--- learn/swift/playground/MyPlayground.playground/Contents.swift
+++ learn/swift/playground/MyPlayground.playground/Contents.swift
@@ -2,3 +2,28 @@ import UIKit
 
 var str = "Hello, playground"
 print(str)
+
+let s = "Hello World!"
+s.uppercased()
+let t = Array(s)
+print(t)
+
+
+let i = true
+if i {
+    print("if")
+} else {
+    print("else")
+}
+
+
+let httpsResponse = (status:200, msg:"ok")
+print(httpsResponse.status, httpsResponse.msg)
+print(httpsResponse.0, httpsResponse.1)
+
+let number = "2"
+if let converted = Int(number) {
+    print(converted)
+} else {
+    print("else")
+}
