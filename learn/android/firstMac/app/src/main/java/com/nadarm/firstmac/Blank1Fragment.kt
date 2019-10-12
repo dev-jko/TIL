027d69518ad151ab4f9c363b801cb7f44899246d
nadarm <ccc7497@gmail.com>
2019-10-12 19:04:19 +0900

    fragment life cycle

---
+++ learn/android/firstMac/app/src/main/java/com/nadarm/firstmac/Blank1Fragment.kt
@@ -0,0 +1,75 @@
+package com.nadarm.firstmac
+
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class Blank1Fragment : Fragment() {
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        println("${this.javaClass.simpleName} - onAttach(context)")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        println("${this.javaClass.simpleName} - onCreate")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        println("${this.javaClass.simpleName} - onCreateView")
+        return inflater.inflate(R.layout.fragment_blank1, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        println("${this.javaClass.simpleName} - onViewCreated")
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        println("${this.javaClass.simpleName} - onActivityCreated")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        println("${this.javaClass.simpleName} - onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        println("${this.javaClass.simpleName} - onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        println("${this.javaClass.simpleName} - onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        println("${this.javaClass.simpleName} - onStop")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        println("${this.javaClass.simpleName} - onDestroyView")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        println("${this.javaClass.simpleName} - onDestroy")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        println("${this.javaClass.simpleName} - onDetach")
+    }
+}
--- learn/android/firstMac/app/src/main/java/com/nadarm/firstmac/Main2Activity.kt
+++ learn/android/firstMac/app/src/main/java/com/nadarm/firstmac/Main2Activity.kt
@@ -2,14 +2,25 @@ package com.nadarm.firstmac
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.activity_main2.*
 
 class Main2Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
-
         println("${this.javaClass.simpleName} - onCreate")
+
+        btn1.setOnClickListener {
+            println("${this.javaClass.simpleName} - btn1 clicked")
+            navHostFragment.findNavController().navigate(R.id.actionBlank1Fragment)
+        }
+
+        btn2.setOnClickListener {
+            println("${this.javaClass.simpleName} - btn2 clicked")
+            navHostFragment.findNavController().navigate(R.id.actionBlank2Fragment)
+        }
     }
 
 
